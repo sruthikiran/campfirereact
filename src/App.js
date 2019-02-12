5b8dbb6234fc4aa25107534d41f4558adea70f5b
Developer <developer@Danes-MacBook-Pro.local>
2019-02-12 12:27:58 -0800

    profile page changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 } from "react-router-dom";
 
 const Landing = () => (
-  <div style={landing_style} className="">
+  <div style={landing_style} className="container">
     <h1>Welcome to Campfire!</h1>
     <p className="flow-text"> One common flaw we've seen in many frameworks is a lack of support for truly responsive text. While elements on the page resize fluidly, text still resizes on a fixed basis. To ameliorate this problem, for text heavy pages, we've created a class that fluidly scales text size and line-height to optimize readability for the user. Line length stays between 45-80 characters and line height scales to be larger on smaller screens.
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import Image from "../../img/dude.png"
 
 export class Profile extends Component {
   render() {
     return (
       <div>
-        <p>Hello from Profile</p>
+        <img className="circle responsive-img" src={Image}></img>
+        <p>Name: Dane Olsen</p>
+        <p>Title: Developer</p>
+        <p>Location: San Jose, CA</p>
+        <p>Email: dane@dane.com</p>
+        <p>Joined on: 1/1/2019</p>
+        <button>Edit Profile</button>
       </div>
     )
   }
