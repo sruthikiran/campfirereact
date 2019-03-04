77e3fe6d368e001c206b2771ce2646a140419ee6
Developer <developer@Danes-MacBook-Pro.local>
2019-03-04 11:58:11 -0800

    Thread component created

--- src/components/Forum/Forums.js
+++ src/components/Forum/Forums.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react'
 import {Navbar, NavItem,Tabs, Tab} from 'react-materialize'
 import QuestionOfDay from './QuestionOfDay'
 import AllTopics from './AllTopics'
+import Thread from './Thread'
 
 export class Forums extends Component {
   render() {
     return (
       <div style={divStyle}>
         <p style={headerStyle}>  <span style={important}> Forum </span> </p>
-        <Tabs style={tabStyle} className="tab-demo z-depth-1 indigo">
+        <Tabs style={tabStyle} className="indigo">
           <Tab style={itemStyle} title="All Topics"> </Tab>
           <Tab style={itemStyle} title="New Topics" active></Tab>
           <Tab style={itemStyle} title="My Topics"></Tab>
         </Tabs>
         <QuestionOfDay />
         <AllTopics />
-
+        <p>---------------------------------------------</p>
+        <Thread />
 
       </div>
     )
@@ -26,12 +28,16 @@ export class Forums extends Component {
 
 const tabStyle={
   marginBottom: '1.5em',
+  color: 'white',
+  backgroundColor: '#3949ab',
   
 }
 
 const itemStyle={
   margin: '1em',
   color: 'white',
+  backgroundColor: '#3949ab',
+  
 }
 
 const divStyle ={
--- src/components/Forum/Thread.js
+++ src/components/Forum/Thread.js
@@ -1,13 +1,81 @@
 import React, { Component } from 'react'
+import MyEditor from '../MyEditor'
+import {Chip, Button} from 'react-materialize'
+import Image from '../../img/dude.png'
 
 export class Thread extends Component {
   render() {
     return (
-      <div>
-        
+      <div style={threadStyle}>
+        <div style={originalPost}>
+            <h4>Thread Title</h4>
+            <p>Posted by: <Chip><img  className="circle" src={Image} /> Dane Olsen</Chip></p>
+            <p>on 3/1/2019 at 4:45pm</p>
+            <p>Tags: <Chip>Python</Chip><Chip>Java</Chip><Chip>OOP</Chip></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <hr style={hrStyle} />
+        <div style={threadResponse}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Posted by:  <Chip><img  className="circle" src={Image} /> Andrew Skinner</Chip></p>
+            <p>on 3/1/2019 at 6:00pm</p>
+            <Button style={btnStyle} className='indigo right'><i className="material-icons">thumb_up</i> +300</Button>
+        </div>
+        <hr style={hrStyle} />
+        <div style={threadResponse}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Posted by:  <Chip><img  className="circle" src={Image} /> Andrew Skinner</Chip></p>
+            <p>on 3/1/2019 at 6:00pm</p>
+            <Button style={btnStyle} className='indigo right'><i className="material-icons">thumb_up</i> +10</Button>
+        </div>
+        <hr style={hrStyle} />
+        <div style={threadResponse}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Posted by:  <Chip><img  className="circle" src={Image} /> Andrew Skinner</Chip></p>
+            <p>on 3/1/2019 at 6:00pm</p>
+            <Button style={btnStyle} className='indigo right'><i className="material-icons">thumb_up</i> +2</Button>
+        </div>
+        <hr style={hrStyle} />
+        <div style={editor}>
+            <MyEditor />
+        </div>
       </div>
     )
   }
 }
+
+const btnStyle={
+    marginTop: '-4em',
+    width: '9em',
+}
+
+const editor={
+
+}
+
+const originalPost={
+    
+}
+
+const threadResponse={
+    marginLeft: '1em',
+}
+
+const imgStyle={
+    height: '1em',
+    width: '1em',
+}
+
+const threadStyle={
+    border: '2px white solid',
+    borderRadius: '5px',
+    padding: '2em',
+    paddingBottom: '5em',
+}
+
+const hrStyle={
+    margin: '1em 0em',
+}
+
 
 export default Thread
